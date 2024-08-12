f21a28f6ac50799439cdfa011104c6b82507ec5c
Anton Klemen <antonklemen@gmail.com>
2024-08-12 11:33:12 +0200

    Cleans up train and infer scripts, fixes paths.

--- speechllm/infer_conformer-ctc_llama_lora.sh
+++ speechllm/infer_conformer-ctc_llama_lora.sh
@@ -1,20 +1,17 @@
-# NeMo
-NEMO_REPOSITORY_PATH="/NeMo"
-NEMO_SPEECHLLM_EXAMPLES_PATH="$NEMO_REPOSITORY_PATH/examples/multimodal/speech_llm"
-
-# dataset
-NEMO_ARTUR_PATH="/dataset/artur/v1.0/nemo"
-TEST_MANIFESTS="[$NEMO_ARTUR_PATH/test.nemo]"
+# manifests
+TEST_MANIFESTS="[/manifests/artur/test.nemo]"
 TEST_NAMES="[artur-test]"
 
-SPEECHLLM_MODEL_PATH="/speechllm_conformer-ctc_llama31_8b_lora.nemo" # TODO - adjust path, if needed
+SPEECHLLM_MODEL_PATH="" # TODO - set path to the SpeechLLM
+
+OUTPUT_DIR="" # TODO - set output dir
 
 # inference
-cd "$NEMO_SPEECHLLM_EXAMPLES_PATH" || exit
+cd "/NeMo/examples/multimodal/speech_llm" || exit
 # TODO - set batch sizes, depending on GPUs usage
 python modular_audio_gpt_eval.py \
     model.restore_from_path="$SPEECHLLM_MODEL_PATH" \
-    model.data.test_ds.manifest_filepath="$TEST_MANIFESTS" \
+    ++model.data.test_ds.manifest_filepath="$TEST_MANIFESTS" \
     model.data.test_ds.names="$TEST_NAMES" \
     model.data.test_ds.global_batch_size=8 \
     model.data.test_ds.micro_batch_size=8 \
@@ -24,4 +21,4 @@ python modular_audio_gpt_eval.py \
     ++inference.top_p=0.95 \
     ++inference.temperature=0.4 \
     ++inference.repetition_penalty=1.2 \
-    ++model.data.test_ds.output_dir="./test_outputs"
+    ++model.data.test_ds.output_dir="$OUTPUT_DIR"
