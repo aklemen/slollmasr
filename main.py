fa909867df31eb293e6a0ac61215b268f7beab42
Anton Klemen <antonklemen@gmail.com>
2024-08-18 16:29:54 +0200

    Removes unused code.

--- main.py
+++ main.py
@@ -7,26 +7,6 @@ from HypothesesDataset import HypothesesDataset
 from Manifest import Manifest
 from llms.LargeLanguageModel import LargeLanguageModel
 
-# def read_transcripts(json_file_path: str) -> list[Transcript]:
-#     with open(json_file_path, 'r') as file:
-#         json_data = json.load(file)
-#
-#     transcripts = []
-#
-#     for item in json_data:
-#         transcript = Transcript(item["hypotheses"], item["transcript"])
-#         transcripts.append(transcript)
-#
-#     return transcripts
-#
-#
-# def run_reevaluation(re_evaluator: ReEvaluator):
-#     transcripts = read_transcripts("hypotheses_sample.json")
-#     for transcript in transcripts:
-#         re_evaluated_transcript = re_evaluator.re_evaluate(transcript.get_hypotheses())
-#         print(re_evaluated_transcript)
-
-
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
     start_time = time.time()
