0c435fe2a03cffdf5a28e4e0feaf1bb0f57fecee
Anton Klemen <antonklemen@gmail.com>
2024-08-20 08:46:20 +0000

    Sets up auto device mapping for LLM.

--- llms/LargeLanguageModel.py
+++ llms/LargeLanguageModel.py
@@ -2,15 +2,14 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
 class LargeLanguageModel:
-    def __init__(self, name: str, device="cuda"):
+    def __init__(self, name: str):
         self.name = name
-        self.device = device
         self.model = (
             AutoModelForCausalLM.from_pretrained(
                 pretrained_model_name_or_path=name,
                 is_decoder=True,
+                device_map="auto"   
             )
-            .to(device)
             .eval()
         )
         self.tokenizer = AutoTokenizer.from_pretrained(
