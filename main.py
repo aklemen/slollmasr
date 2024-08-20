50ebeb9ce1647c49242c3aa7a7b0bcbc7486800c
Anton Klemen <antonklemen@gmail.com>
2024-08-20 16:10:40 +0000

    Allows passing parameters to main script.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from argparse import ArgumentParser
 import logging
+import os
+from pathlib import Path
 import time
 import pandas as pd
 
@@ -10,37 +13,59 @@ from llms.LargeLanguageModel import LargeLanguageModel
 from BestHypothesesSelector import BestHypothesesSelector
 
 if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.INFO)
-    start_time = time.time()
+    parser = ArgumentParser()
+    parser.add_argument('--llm_name', type=str, required=True)
+    parser.add_argument('--beams_file_path', type=str, required=True)
+    parser.add_argument('--manifest_file_path', type=str, required=True)
+    parser.add_argument('--beam_size', type=int, required=True)
+    parser.add_argument('--results_dir_path', type=str, required=True)
+    parser.add_argument('--log', type=bool, default=False)
+    args = parser.parse_args()
+    
+    if (args.log):
+        logging.getLogger().setLevel(logging.INFO)
+        
+    
+    start_time = time.time()    
 
-    hypotheses = pd.read_csv("/hf-home/beams_output/preds_out_width5_alpha1.0_beta0.0.tsv", delimiter="\t", header=None, names=["text", "score"])
-    manifest = Manifest("/dataset/artur/v1.0/nemo/test.nemo")
+    hypotheses = pd.read_csv(args.beams_file_path, delimiter="\t", header=None, names=["text", "score"])
+    manifest = Manifest(args.manifest_file_path)
     ground_truths = manifest.get_transcripts()
 
-    hf_llms = [
-        "openai-community/gpt2",
-        "meta-llama/Meta-Llama-3.1-8B",
-        "google-bert/bert-base-uncased",
-        "FacebookAI/xlm-roberta-base",
-        "meta-llama/Llama-2-7b-chat-hf",
-    ]
-    llm = LargeLanguageModel(hf_llms[1])
+    llm = LargeLanguageModel(args.llm_name)
+    dataset = HypothesesDataset(hypotheses, ground_truths, llm.tokenizer, args.beam_size, 512)
     rescorer = CausalReScorer(llm)
-
-    dataset = HypothesesDataset(hypotheses, ground_truths, llm.tokenizer, 5, 512)
-
+    
     new_scores = rescorer.re_score(dataset)
 
-    old_best = BestHypothesesSelector.select(dataset)
-    new_best = BestHypothesesSelector.select(dataset, new_scores)
+    old_best_hypotheses, old_best_scores = BestHypothesesSelector.select(dataset)
+    new_best_hypotheses, new_best_scores = BestHypothesesSelector.select(dataset, new_scores)
 
     calc = MetricsCalculator()
-    old_wer_score = calc.calculate_wer(predictions=old_best, references=ground_truths)
-    new_wer_score = calc.calculate_wer(predictions=new_best, references=ground_truths)
-
-    print(f"Scores length: {len(new_scores)}")
-    print(f"First 5 score: {new_scores[:5]}")
-    print(f"First 5 hypothesis: {new_best[:5]}")
+    old_wer_score = calc.calculate_wer(old_best_hypotheses, ground_truths)
+    new_wer_score = calc.calculate_wer(new_best_hypotheses, ground_truths)
+    
+    
+    Path(args.results_dir_path).mkdir(parents=True, exist_ok=True)
+    out_file_name = f'rescored_{args.llm_name}_{args.beam_size}.tsv'.replace('/', '_')
+    out_file = f'{args.results_dir_path}/{out_file_name}'
+    data = {
+        'ground_truth': ground_truths,
+        'asr_best_hypothesis': old_best_hypotheses,
+        'asr_llm_best_hypothesis': new_best_hypotheses,
+        'asr_score': old_best_scores,
+        'asr_llm_score': new_best_scores
+    }
+    
+    data = df = pd.DataFrame(data)
+    df.to_csv(out_file, sep='\t', index=False)
+    
+    
     print(f"Old WER: {old_wer_score}")
     print(f"New WER: {new_wer_score}")
     print(f"Execution time: {time.time() - start_time} seconds")
+    print('ground_truths: ', ground_truths[0])
+    print('old_best_hypotheses: ', old_best_hypotheses[0])
+    print('new_best_hypotheses: ', new_best_hypotheses[0])
+    print('old_best_scores: ', old_best_scores[0])
+    print('new_best_scores: ', new_best_scores[0])
