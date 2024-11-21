Fixes WER calculation.

methods/CausalReScorer.py
@@ -87,13 +87,15 @@ class CausalReScorer:
 
     def _find_best_coefficient(self, dataset: HypothesesDataset, scores1, scores2, distances):
         ground_truths_char_lengths_sum = sum(len(ground_truth) for ground_truth in dataset.get_ground_truths())
+        ground_truths_char_lengths_sum = torch.tensor(ground_truths_char_lengths_sum).to(self.device_to_map_to)
         coefficients = self._get_coefficients(scores1, scores2)
         best_coefficient = coefficients[0]
         best_wer = 10000
         for coefficient in coefficients:
             new_scores = scores1 + coefficient * scores2
-            new_best_hypotheses, _, new_best_indices = BestHypothesesSelector.select(dataset, new_scores.tolist())
-            best_hypothesis_distances = distances.gather(index=new_best_indices)
+            _, _, new_best_indices = BestHypothesesSelector.select(dataset, new_scores.tolist())
+            new_best_indices = torch.tensor(new_best_indices).to(self.device_to_map_to)
+            best_hypothesis_distances = distances.gather(dim=0, index=new_best_indices)
             wer = self._calculate_wer(best_hypothesis_distances, ground_truths_char_lengths_sum)
             if wer < best_wer:
                 best_coefficient = coefficient