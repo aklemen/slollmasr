d8c8f854cc1938f9508faacd96c859f2276b66aa
Anton Klemen <a.klemen@sportradar.com>
2025-01-03 00:25:32 +0100

    Sets the total on tqdm.

--- methods/PromptErrorCorrector.py
+++ methods/PromptErrorCorrector.py
@@ -50,7 +50,7 @@ class PromptErrorCorrector(Method):
     def run(self, dataset: HypothesesDataset) -> list[str]:
         prompts_dataset = self._build_prompts_dataset(dataset)
         best_hypotheses = []
-        for sequences in tqdm(self._generator(prompts_dataset, batch_size=8)):
+        for sequences in tqdm(self._generator(prompts_dataset, batch_size=8), total=dataset.get_num_of_samples()):
             output = sequences[-1]["generated_text"]
             sanitized_result = self._sanitize_llm_output(output)
             best_hypotheses.append(sanitized_result)
