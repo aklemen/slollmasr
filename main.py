5593eed8244cf42bfaf6569ce383539000619d85
Anton Klemen <a.klemen@sportradar.com>
2025-01-02 18:46:09 +0100

    Adds PromptErrorCorrector.

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from BestHypothesesSelector import BestHypothesesSelector
 from LargeLanguageModel import LargeLanguageModel
 from MetricsCalculator import MetricsCalculator
 from Tokenizer import Tokenizer
+from methods.CausalReScorer import CausalReScorer
+from methods.PromptErrorCorrector import PromptErrorCorrector
 from methods.PromptRescorer import PromptRescorer
 from parse_args import parse_args
-from methods.CausalReScorer import CausalReScorer
 from torch_datasets.HypothesesDataset import HypothesesDataset
 from torch_datasets.ManifestDataset import ManifestDataset
-
 
 if __name__ == '__main__':
     args = parse_args()
@@ -40,13 +40,15 @@ if __name__ == '__main__':
         method = CausalReScorer(llm, tokenizer)
     elif args.method == 'prompt-rescorer':
         method = PromptRescorer(llm, tokenizer)
+    elif args.method == 'prompt-error-corrector':
+        method = PromptRescorer(llm, tokenizer)
     else:
         raise Exception(f"Method {args.method} is not implemented!")
 
     manifest = ManifestDataset(args.manifest_file_path)
     ground_truths = manifest.get_transcripts()
 
-    results_file_base = f'{args.method}_{args.llm_name}'.replace('/', '_')
+    results_file_basename = f'{args.method}_{args.llm_name}'.replace('/', '_')
     wers_df = pd.DataFrame(columns=['beam_size', 'old_wer', 'new_wer', 'rescoring_duration'])
 
     for beam_size, beams_file_path, alpha, beta in zip(args.beam_sizes, args.beams_file_paths, args.alphas, args.betas):
@@ -54,44 +56,57 @@ if __name__ == '__main__':
         hypotheses = pd.read_csv(beams_file_path, delimiter="\t", header=None, names=["text", "score"])
         dataset = HypothesesDataset(hypotheses, ground_truths)
 
-        start_time = time.time()
+        old_best_hypotheses, old_best_scores, old_best_indices = BestHypothesesSelector.select(dataset)
 
-        results_file_name = f'{results_file_base}_{beam_size}'
+        results_file_name = f'{results_file_basename}_{beam_size}'
+        results_dictionary = {
+            'ground_truth': ground_truths,
+            'asr_score': old_best_scores,
+            'asr_best_index': old_best_indices,
+            'asr_best_hypothesis': old_best_hypotheses,
+        }
+
+        start_time = time.time()
         if isinstance(method, CausalReScorer):
             new_scores, used_alpha, used_beta = method.run(dataset, alpha, beta)
+            run_duration = time.time() - start_time
+            new_best_hypotheses, new_best_scores, new_best_indices = BestHypothesesSelector.select(dataset, new_scores)
+            results_dictionary.update({
+                'asr+llm_score': new_best_scores,
+                'asr+llm_best_index': new_best_indices,
+            })
             results_file_name += f'_alpha={used_alpha}_beta={used_beta}'
         elif isinstance(method, PromptRescorer):
             new_scores = method.run(dataset)
+            run_duration = time.time() - start_time
+            new_best_hypotheses, new_best_scores, new_best_indices = BestHypothesesSelector.select(dataset, new_scores)
+            results_dictionary.update({
+                'asr+llm_score': new_best_scores,
+                'asr+llm_best_index': new_best_indices,
+            })
+        elif isinstance(method, PromptErrorCorrector):
+            new_best_hypotheses = method.run(dataset)
+            run_duration = time.time() - start_time
         else:
             raise Exception(f"Method {method} is not handled!")
 
-        rescoring_duration = time.time() - start_time
-
-        old_best_hypotheses, old_best_scores, old_best_indices = BestHypothesesSelector.select(dataset)
-        new_best_hypotheses, new_best_scores, new_best_indices = BestHypothesesSelector.select(dataset, new_scores)
-        old_wer_score = calc.calculate_wer(old_best_hypotheses, ground_truths)
-        new_wer_score = calc.calculate_wer(new_best_hypotheses, ground_truths)
-
+        results_dictionary.update({
+            'asr+llm_best_hypothesis': new_best_hypotheses,
+        })
 
         results_file_path = f'{args.results_dir_path}/{results_file_name}.tsv'
-        data = df = pd.DataFrame({
-            'ground_truth': ground_truths,
-            'asr_best_hypothesis': old_best_hypotheses,
-            'asr_llm_best_hypothesis': new_best_hypotheses,
-            'asr_score': old_best_scores,
-            'asr_llm_score': new_best_scores,
-            'asr_best_index': old_best_indices,
-            'asr_llm_best_index': new_best_indices,
-        })
-        df.to_csv(results_file_path, sep='\t', index=False)
+        results_df = pd.DataFrame(results_dictionary)
+        results_df.to_csv(results_file_path, sep='\t', index=False)
         print(f"Results saved to {results_file_path}!")
 
+        old_wer_score = calc.calculate_wer(old_best_hypotheses, ground_truths)
+        new_wer_score = calc.calculate_wer(new_best_hypotheses, ground_truths)
         new_wer_df = pd.DataFrame({
             'beam_size': [beam_size],
             'old_wer': [old_wer_score],
             'new_wer': [new_wer_score],
-            'rescoring_duration': [rescoring_duration],
+            'run_duration': [run_duration],
         })
         wers_df = pd.concat([wers_df, new_wer_df], ignore_index=True)
-        wers_df.to_csv(f'{args.results_dir_path}/{results_file_base}_wers.tsv', sep='\t', index=False)
+        wers_df.to_csv(f'{args.results_dir_path}/{results_file_basename}_wers.tsv', sep='\t', index=False)
         print(wers_df.to_string())
---
+++ methods/PromptErrorCorrector.py
@@ -0,0 +1,59 @@
+import torch
+from transformers import pipeline
+
+from LargeLanguageModel import LargeLanguageModel
+from Tokenizer import Tokenizer
+from methods.Method import Method
+from torch_datasets.HypothesesDataset import HypothesesDataset
+
+
+class PromptErrorCorrector(Method):
+    def __init__(self, llm: LargeLanguageModel, tokenizer: Tokenizer):
+        super().__init__(llm, tokenizer)
+        self._generator = pipeline(
+            "text-generation",
+            model=llm.model,
+            tokenizer=tokenizer.tokenizer,
+            device_map="auto",
+            torch_dtype="auto",
+            num_return_sequences=1,
+            max_new_tokens=512,
+            return_full_text=False,
+        )
+        self.should_use_chat_templates = tokenizer.are_chat_templates_supported()
+
+    def run(self, dataset: HypothesesDataset) -> list[str]:
+        best_hypotheses = []
+        for sample_idx in range(dataset.get_num_of_samples()):
+            prompt = self._generate_prompt(dataset, sample_idx)
+            sequences = self._generator(prompt)
+            result = sequences[-1]["generated_text"]
+            best_hypotheses.append(result)
+
+        return best_hypotheses
+
+    def _generate_prompt(self, dataset, sample_idx):
+        beam_size = dataset.get_beam_size()
+
+        hypotheses_list = ""
+        from_idx = sample_idx * beam_size
+        to_idx = (sample_idx + 1) * beam_size
+        indexes = list(range(from_idx, to_idx))
+
+        for i in range(beam_size):
+            hypothesis_index = indexes[i]
+            hypothesis = dataset[hypothesis_index][0]
+            hypotheses_list += f'<hipoteza{i}> {hypothesis} </hipoteza{i}>\n'
+
+        prompt_start = (
+            f"Izvedi popravljanje napak na najboljših {beam_size} izhodih, ki jih je generiral sistem za samodejno razpoznavanje govora (Automatic Speech Recognition). "
+            f"Hipoteze, navedene po vrstnem redu glede na njihovo posteriorno verjetnost sistema ASR, so naslednje:\n\n"
+        )
+
+        prompt_end = "Prosim, izpiši popravljen najboljši transkript danega govora, brez dodatnih razlag ali besed."
+
+        prompt = prompt_start + hypotheses_list + prompt_end
+
+        if self.should_use_chat_templates:
+            return [{ "role": "user", "content": prompt }]
+        return prompt
