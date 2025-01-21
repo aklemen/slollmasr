e4d811380ec8f384bc8fd1848574a51a8a75acd3
Anton Klemen <a.klemen@sportradar.com>
2025-01-21 11:46:44 +0100

    Adds slovenianGPT script, fixes beam sizes missing.

---
+++ slurm/causal-rescore/gordicaleksa_SlovenianGPT.sh
@@ -0,0 +1,7 @@
+export LLM_NAME="gordicaleksa/SlovenianGPT"
+export TOKENIZER_NAME="mistralai/Mistral-7B-v0.1"
+export ALPHAS="0.797 0.702 0.822 0.826"
+export BETAS="-0.037 0.134 0.415 0.520"
+export BATCH_SIZE="128"
+
+sbatch causal-rescore-gams.sbatch
