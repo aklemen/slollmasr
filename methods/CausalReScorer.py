0cb1ccb6ef50d9274d6facaf07ab14387ff9fce0
Anton Klemen <a.klemen@sportradar.com>
2025-01-22 10:13:53 +0100

    Removes autocasting - doesn't work with Gemma2 and most models are bfloat16 anyway.

--- methods/CausalReScorer.py
+++ methods/CausalReScorer.py
@@ -41,42 +41,41 @@ class CausalReScorer(Method):
             Logger.info(f'Attention mask NOT supported by "{self.llm_name}" and will NOT be used.')
             support_attention_mask = False
 
-        with torch.amp.autocast('cuda'):
-            with torch.no_grad():
-                hypotheses, asr_scores, llm_scores, char_lengths, distances = [], [], [], [], []
-                for batch in tqdm(data_loader):
-                    _, asr_score, input_ids, input_mask, char_length, distance = batch
+        with torch.no_grad():
+            hypotheses, asr_scores, llm_scores, char_lengths, distances = [], [], [], [], []
+            for batch in tqdm(data_loader):
+                _, asr_score, input_ids, input_mask, char_length, distance = batch
 
-                    max_len_in_batch = input_mask.sum(dim=0).argmin().item()
-                    input_ids, input_mask = input_ids[:, :max_len_in_batch], input_mask[:, :max_len_in_batch]
-                    input_ids, input_mask = input_ids.to(self.device_to_map_to), input_mask.to(self.device_to_map_to)
-                    asr_score = asr_score.to(self.device_to_map_to)
-                    char_length = char_length.to(self.device_to_map_to)
-                    distance = distance.to(self.device_to_map_to)
+                max_len_in_batch = input_mask.sum(dim=0).argmin().item()
+                input_ids, input_mask = input_ids[:, :max_len_in_batch], input_mask[:, :max_len_in_batch]
+                input_ids, input_mask = input_ids.to(self.device_to_map_to), input_mask.to(self.device_to_map_to)
+                asr_score = asr_score.to(self.device_to_map_to)
+                char_length = char_length.to(self.device_to_map_to)
+                distance = distance.to(self.device_to_map_to)
 
-                    if support_attention_mask:
-                        output = self.llm(input_ids=input_ids, attention_mask=input_mask)
-                    else:
-                        output = self.llm(input_ids=input_ids)
+                if support_attention_mask:
+                    output = self.llm(input_ids=input_ids, attention_mask=input_mask)
+                else:
+                    output = self.llm(input_ids=input_ids)
 
-                    log_probs = torch.nn.functional.log_softmax(output.logits, dim=-1)  # [batch_size, sequence_length, vocab_size]
+                log_probs = torch.nn.functional.log_softmax(output.logits, dim=-1)  # [batch_size, sequence_length, vocab_size]
 
-                    # We don't care about the last probabilities, because they are for the next token (outside our sequence)
-                    log_probs_without_last = log_probs[:, :-1]  # [batch_size, sequence_length - 1, vocab_size]
+                # We don't care about the last probabilities, because they are for the next token (outside our sequence)
+                log_probs_without_last = log_probs[:, :-1]  # [batch_size, sequence_length - 1, vocab_size]
 
-                    # We shift the IDs to align them with log probabilities (so probabilities at position 0 are for token at position 0)
-                    shifted_input_ids = input_ids[:, 1:]  # [batch_size, sequence_length - 1]
+                # We shift the IDs to align them with log probabilities (so probabilities at position 0 are for token at position 0)
+                shifted_input_ids = input_ids[:, 1:]  # [batch_size, sequence_length - 1]
 
-                    # We need to only get the probabilities of our input tokens (not across the whole vocabulary)
-                    target_log_probs = log_probs_without_last.gather(2, shifted_input_ids.unsqueeze(2)).squeeze(2)
+                # We need to only get the probabilities of our input tokens (not across the whole vocabulary)
+                target_log_probs = log_probs_without_last.gather(2, shifted_input_ids.unsqueeze(2)).squeeze(2)
 
-                    shifted_input_mask = input_mask[:, 1:] # [batch_size, sequence_length - 1]
-                    llm_score = torch.sum(target_log_probs * shifted_input_mask, dim=-1)
+                shifted_input_mask = input_mask[:, 1:] # [batch_size, sequence_length - 1]
+                llm_score = torch.sum(target_log_probs * shifted_input_mask, dim=-1)
 
-                    asr_scores.append(asr_score)
-                    llm_scores.append(llm_score)
-                    char_lengths.append(char_length)
-                    distances.append(distance)
+                asr_scores.append(asr_score)
+                llm_scores.append(llm_score)
+                char_lengths.append(char_length)
+                distances.append(distance)
 
         asr_scores = torch.cat(asr_scores)
         llm_scores = torch.cat(llm_scores)
