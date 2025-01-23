e8f236b83fa25853cc2de9fe476c831c9612773e
Anton Klemen <a.klemen@sportradar.com>
2025-01-23 07:43:02 +0100

    Adds script for Llama-3.1-70B.

---
+++ slurm/causal-rescore/meta-llama_Llama-3.1-70B.sh
@@ -0,0 +1,7 @@
+export LLM_NAME="meta-llama/Llama-3.1-70B"
+export TOKENIZER_NAME=$LLM_NAME
+export ALPHAS="0.404 0.389 0.388 0.404"
+export BETAS="0.093 0.150 0.165 0.112"
+export BATCH_SIZE="32"
+
+sbatch causal-rescore.sbatch
