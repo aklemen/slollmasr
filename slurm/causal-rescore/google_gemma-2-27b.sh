534abfbb99f29ad704f9783c2a0ba1b3a5d36479
Anton Klemen <a.klemen@sportradar.com>
2025-01-23 13:57:41 +0100

    Uses correct minus sign.

--- slurm/causal-rescore/google_gemma-2-27b.sh
+++ slurm/causal-rescore/google_gemma-2-27b.sh
@@ -1,7 +1,7 @@
 export LLM_NAME="google/gemma-2-27b"
 export TOKENIZER_NAME=$LLM_NAME
 export ALPHAS="0.386 0.388 0.446 0.462"
-export BETAS="−0.029 −0.001 −0.005 0.001"
+export BETAS="-0.029 -0.001 -0.005 0.001"
 export BATCH_SIZE="32"
 
 sbatch causal-rescore.sbatch
