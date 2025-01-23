fdabd7b6876baf00f86d20d4bd12749bfb15ce6a
Anton Klemen <a.klemen@sportradar.com>
2025-01-23 14:01:31 +0100

    Uses new alphas and betas for google/gemma-2-9b.

--- slurm/causal-rescore/google_gemma-2-9b.sh
+++ slurm/causal-rescore/google_gemma-2-9b.sh
@@ -1,9 +1,7 @@
 export LLM_NAME="google/gemma-2-9b"
 export TOKENIZER_NAME=$LLM_NAME
-export ALPHAS="0.297 0.297 0.297 0.297"
-export BETAS="0.457 0.216 -0.07 0.213"
+export ALPHAS="0.311 0.298 0.295 0.294"
+export BETAS="0.463 0.222 -0.072 0.254"
 export BATCH_SIZE="64"
 
 sbatch causal-rescore.sbatch
-
-
