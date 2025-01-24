58640fecd08c209a93d07e1d4bec137ba77519a5
Anton Klemen <a.klemen@sportradar.com>
2025-01-24 15:19:45 +0100

    Tries to reduce batch size to avoid error.
    RuntimeError: CUDA error: unspecified launch failure
    CUDA kernel errors might be asynchronously reported at some other API call, so the stacktrace below might be incorrect.
    For debugging consider passing CUDA_LAUNCH_BLOCKING=1
    Compile with `TORCH_USE_CUDA_DSA` to enable device-side assertions.

--- slurm/causal-rescore/google_gemma-2-27b.sh
+++ slurm/causal-rescore/google_gemma-2-27b.sh
@@ -2,6 +2,6 @@ export LLM_NAME="google/gemma-2-27b"
 export TOKENIZER_NAME=$LLM_NAME
 export ALPHAS="0.386 0.388 0.446 0.462"
 export BETAS="-0.029 -0.001 -0.005 0.001"
-export BATCH_SIZE="32"
+export BATCH_SIZE="16"
 
 sbatch --job-name=causal-rescore-gemma-2-27b causal-rescore.sbatch
