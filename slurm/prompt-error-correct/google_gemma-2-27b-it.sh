f930470683016c8c36526161e54b26466064bb18
Anton Klemen <a.klemen@sportradar.com>
2025-01-25 12:35:00 +0100

    Adds script for Gemma 2 9B IT.

--- slurm/prompt-error-correct/google_gemma-2-27b-it.sh
+++ slurm/prompt-error-correct/google_gemma-2-27b-it.sh
@@ -1,5 +1,5 @@
 export LLM_NAME="google/gemma-2-27b-it"
 export TOKENIZER_NAME=$LLM_NAME
-export BATCH_SIZE="128"
+export BATCH_SIZE=128
 
 sbatch --job-name=prompt-error-correct-gemma-2-27b-it prompt-error-correct.sbatch
--- slurm/prompt-error-correct/meta-llama_Llama-3.1-8B-Instruct.sh
+++ slurm/prompt-error-correct/meta-llama_Llama-3.1-8B-Instruct.sh
@@ -1,5 +1,5 @@
 export LLM_NAME="meta-llama/Llama-3.1-8B-Instruct"
 export TOKENIZER_NAME=$LLM_NAME
-export BATCH_SIZE="32"
+export BATCH_SIZE=128
 
 sbatch --job-name=prompt-error-correct-llama-31-8b-instruct prompt-error-correct.sbatch
