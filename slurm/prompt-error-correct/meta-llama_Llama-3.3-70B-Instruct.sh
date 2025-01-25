01e0deed0d116dfad0c3e203b5d4f88941da2360
Anton Klemen <a.klemen@sportradar.com>
2025-01-25 10:41:39 +0100

    Reduces batch size to avoid OOM.

--- slurm/prompt-error-correct/meta-llama_Llama-3.3-70B-Instruct.sh
+++ slurm/prompt-error-correct/meta-llama_Llama-3.3-70B-Instruct.sh
@@ -1,5 +1,5 @@
 export LLM_NAME="meta-llama/Llama-3.3-70B-Instruct"
 export TOKENIZER_NAME=$LLM_NAME
-export BATCH_SIZE=256
+export BATCH_SIZE=128
 
 sbatch --job-name=prompt-error-correct-llama-33-70b-instruct prompt-error-correct.sbatch
