431719e101e542b44c0c549f1e875d3fd056e344
Anton Klemen <a.klemen@sportradar.com>
2025-02-15 12:29:50 +0100

    Fixes param name.

--- scripts/whisper.sh
+++ scripts/whisper.sh
@@ -4,5 +4,5 @@ TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
 python /slollmasr/whisper_transcribe.py \
   --manifest_file_path "/dataset/artur/v1.0/nemo/test.nemo" \
-  --beams_file_dir "/testing/beams_$TIMESTAMP.tsv" \
+  --beams_file_path "/testing/beams_$TIMESTAMP.tsv" \
   --beam_width 5
