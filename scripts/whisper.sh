2c469d9d06ec11880d87737b90f66654294f227e
Anton Klemen <a.klemen@sportradar.com>
2025-02-15 12:33:09 +0100

    Checks if whisper is installed.

--- scripts/whisper.sh
+++ scripts/whisper.sh
@@ -2,6 +2,11 @@
 
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
+if ! pip show whisper; then
+  echo "whisper package is not installed!"
+  exit 1
+fi
+
 python /slollmasr/whisper_transcribe.py \
   --manifest_file_path "/dataset/artur/v1.0/nemo/test.nemo" \
   --beams_file_path "/testing/beams_$TIMESTAMP.tsv" \
