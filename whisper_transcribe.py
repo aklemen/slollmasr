0f93c8a420b480780b601d9b441ff61affeeb504
Anton Klemen <a.klemen@sportradar.com>
2025-02-26 11:53:42 +0100

    Refactors the whisper transcription script.

--- whisper_transcribe.py
+++ whisper_transcribe.py
@@ -7,25 +7,49 @@ import torch
 import whisper
 from tqdm import tqdm
 
+from logger import Logger
 from metrics_calculator import MetricsCalculator
 from whisper.normalizers import BasicTextNormalizer
 
+class WhisperTranscriber:
+    def __init__(self):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = whisper.load_model(name="base", device=self.device)
+        self.normalizer = BasicTextNormalizer()
 
-def get_best_n_indices(best_hypotheses: list[str], n: int) -> list[int]:
-    best_n_indices: list[int] = []
+    def transcribe(self, audio_filepath: str, beam_width: int) -> tuple[list[str], list[float]]:
+        audio = whisper.load_audio(audio_filepath)
+        audio = whisper.pad_or_trim(audio)
+        mel = whisper.log_mel_spectrogram(audio).to(self.device)
+        options = whisper.DecodingOptions(language="sl", beam_size=50)
+        texts, log_probs = whisper.decode(self.model, mel, options)
+        texts = [self.normalizer(text) for text in texts]
 
-    best_n_hypotheses: list[str] = []
-    for idx, hypothesis in enumerate(best_hypotheses):
-        if len(best_n_indices) < n and len(hypothesis) > 0 and hypothesis not in best_n_hypotheses:
-            best_n_hypotheses.append(hypothesis)
-            best_n_indices.append(idx)
+        indices = self._get_best_n_indices(texts, beam_width)
 
-    if len(best_n_indices) < n:
-        for _ in range(n - len(best_n_hypotheses)):
-            random_idx_to_use_again = random.choice(best_n_indices)
-            best_n_indices.append(random_idx_to_use_again)
+        unsorted_best_texts = [texts[i] for i in indices]
+        unsorted_best_log_probs = [log_probs[i] for i in indices]
 
-    return best_n_indices
+        sorted_pairs = sorted(zip(unsorted_best_texts, unsorted_best_log_probs), key=lambda x: x[1], reverse=True)
+        sorted_best_texts, sorted_best_log_probs = [list(t) for t in zip(*sorted_pairs)]
+
+        return sorted_best_texts, sorted_best_log_probs
+
+    def _get_best_n_indices(self, best_hypotheses: list[str], n: int) -> list[int]:
+        best_n_indices: list[int] = []
+
+        best_n_hypotheses: list[str] = []
+        for idx, hypothesis in enumerate(best_hypotheses):
+            if len(best_n_indices) < n and len(hypothesis) > 0 and hypothesis not in best_n_hypotheses:
+                best_n_hypotheses.append(hypothesis)
+                best_n_indices.append(idx)
+
+        if len(best_n_indices) < n:
+            for _ in range(n - len(best_n_hypotheses)):
+                random_idx_to_use_again = random.choice(best_n_indices)
+                best_n_indices.append(random_idx_to_use_again)
+
+        return best_n_indices
 
 
 if __name__ == '__main__':
@@ -35,47 +59,29 @@ if __name__ == '__main__':
     parser.add_argument('--beam_width', type=int, help='Width of the resulting beams')
     args = parser.parse_args()
 
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = whisper.load_model(name="base", device=device)
-    normalizer = BasicTextNormalizer()
+    with open(args.manifest_file_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    transcriber = WhisperTranscriber()
     calc = MetricsCalculator()
 
     hypotheses_list = []
     asr_scores_list = []
-    count = 0
     wer = 0
-
-    with open(args.manifest_file_path, "r", encoding="utf-8") as f:
-        lines = f.readlines()
 
     for line in tqdm(lines):
         manifest_entry = json.loads(line)
-        audio = whisper.load_audio(manifest_entry["audio_filepath"])
-        audio = whisper.pad_or_trim(audio)
-        mel = whisper.log_mel_spectrogram(audio).to(model.device)
-        options = whisper.DecodingOptions(language="sl", beam_size=50)
-        texts, log_probs = whisper.decode(model, mel, options)
-        texts = [normalizer(text) for text in texts]
+        best_texts, best_log_probs = transcriber.transcribe(manifest_entry["audio_filepath"], args.beam_width)
 
-        indices = get_best_n_indices(texts, args.beam_width)
+        hypotheses_list.extend(best_texts)
+        asr_scores_list.extend(best_log_probs)
 
-        best_texts = [texts[i] for i in indices]
-        best_log_probs = [log_probs[i] for i in indices]
-
-        sorted_pairs = sorted(zip(best_texts, best_log_probs), key=lambda x: x[1], reverse=True)
-        sorted_best_texts, sorted_best_log_probs = [list(t) for t in zip(*sorted_pairs)]
-
-        hypotheses_list.extend(sorted_best_texts)
-        asr_scores_list.extend(sorted_best_log_probs)
-
-        count += 1
-        current_wer = calc.calculate_wer([sorted_best_texts[0]], [manifest_entry["text"]])
-        print(f"Current WER: {current_wer}")
+        current_wer = calc.calculate_wer([best_texts[0]], [manifest_entry["text"]])
         wer += current_wer
 
     df = pd.DataFrame({"hypotheses": hypotheses_list, "asr_scores": asr_scores_list})
     df.to_csv(args.beams_file_path, sep='\t', index=False, header=False)
 
-    print(f"Saved beams to {args.beams_file_path}")
-    print(f'WER = {wer / count}')
+    Logger.info(f"Saved beams to {args.beams_file_path}")
+    Logger.info(f'WER = {wer / len(lines)}')
 
