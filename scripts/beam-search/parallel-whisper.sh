93a886572a843514c4faaa60d1287e2e53b97b3a
Anton Klemen <a.klemen@sportradar.com>
2025-02-27 10:24:04 +0100

    Adjusts whisper scripts.

--- scripts/beam-search/parallel-whisper.sh
+++ scripts/beam-search/parallel-whisper.sh
@@ -7,10 +7,10 @@ if ! pip show whisper; then
   /slollmasr/scripts/beam-search/install-whisper.sh
 fi
 
-RESULTS_DIR="/testing/whisper/$TIMESTAMP"
+RESULTS_DIR="/testing/parallel-whisper/$TIMESTAMP"
 mkdir -p "$RESULTS_DIR"
 
-python /slollmasr/whisper_transcribe.py \
+python /slollmasr/parallel_whisper_transcribe.py \
   --manifest_file_path "/dataset/artur/v1.0/nemo/clean/train.nemo" \
   --results_dir_path "$RESULTS_DIR" \
   --beam_width 10
