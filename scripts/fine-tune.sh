ec870da1c8fc470d4621b1235d0c4dda99f3e9e0
Anton Klemen <a.klemen@sportradar.com>
2025-03-23 19:56:42 +0100

    Adds a test fine tune script.

---
+++ scripts/fine-tune.sh
@@ -0,0 +1,9 @@
+WANDB_API_KEY-"<your_wandb_api_key>"
+
+python /slollmasr/fine_tune.py \
+--llm_name "google/gemma-2-9b" \
+--manifest_file_path "/beams/whisper/artur/train/transcribed_manifest.nemo" \
+--beams_file_path "/beams/whisper/artur/train/beams_10.tsv" \
+--beam_size 10 \
+--output_dir_path "/testing/fine-tune" \
+--run_name "test-run"
