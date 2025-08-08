53c8a836ccbdce82d940c3df716432a4b93c9e05
Anton Klemen <a.klemen@sportradar.com>
2025-08-08 16:23:46 +0200

    Adds builder script for CTC-only dataset.

---
+++ scripts/datasets/build_prompt_completion_dataset_ctc.sh
@@ -0,0 +1,4 @@
+python /slollmasr/scripts/datasets/build_prompt_completion_dataset_ctc.py \
+  --manifest_file_path "/dataset/artur/v1.0/nemo/clean/train.nemo" \
+  --beams_file_path "/exp/beam-search/2025-01-28___08-15-19_454/beams/artur/v1.0/nemo/clean/train/preds_out_width10_alpha1.0_beta0.0.tsv" \
+  --output_dir_path "/testing/prompt-completion-dataset"
