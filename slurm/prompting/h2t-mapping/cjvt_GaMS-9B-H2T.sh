27fab608059f5f54460bfc00c05dd8d00a99ae55
Anton Klemen <a.klemen@sportradar.com>
2025-08-12 22:59:51 +0200

    Updates script with the newly trained model.

--- slurm/prompting/h2t-mapping/cjvt_GaMS-9B-H2T.sh
+++ slurm/prompting/h2t-mapping/cjvt_GaMS-9B-H2T.sh
@@ -1,5 +1,5 @@
-export LLM_NAME="aklemen/H2T-LoRA-test"
-export TOKENIZER_NAME="cjvt/GaMS-9B"
+export LLM_NAME="aklemen/H2T-LoRA"
+export TOKENIZER_NAME=$LLM_NAME
 export BATCH_SIZE=128
 
 sbatch --job-name=h2t-mapping-cjvt-GaMS-9B-H2T h2t-mapping.sbatch
