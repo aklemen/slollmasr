1cd41c3333d000601efef5cea38643d3d30c396a
Anton Klemen <a.klemen@sportradar.com>
2025-08-13 21:53:39 +0200

    Does not pad hypotheses if pad_id is not provided.

--- torch_datasets/hypotheses_with_ids_dataset.py
+++ torch_datasets/hypotheses_with_ids_dataset.py
@@ -1,7 +1,8 @@
 import editdistance
-import pandas as pd
 import numpy as np
+import pandas as pd
 
+from logger import Logger
 from torch_datasets.hypotheses_dataset import HypothesesDataset
 
 
@@ -11,7 +12,7 @@ class HypothesesWithIdsDataset(HypothesesDataset):
             hypotheses: pd.DataFrame,
             ground_truths: list[str],
             hypotheses_ids: list[list[int]],
-            pad_id: int,
+            pad_id: int = None,  # If None, no padding and truncation will be applied
             max_seq_length: int = 256,
     ):
         super().__init__(hypotheses, ground_truths)
@@ -31,10 +32,15 @@ class HypothesesWithIdsDataset(HypothesesDataset):
 
     def _get_hypothesis_input_ids(self, idx):
         hypothesis_ids = self.hypotheses_ids[idx]
+
+        if self.pad_id is None:
+            return np.array(hypothesis_ids)
+
+        if len(hypothesis_ids) > self.max_seq_length:
+            Logger.warn(f"Hypothesis at {idx} is longer ({len(hypothesis_ids)}) than max_seq_length ({self.max_seq_length}) and will be truncated.")
         input_ids = [self.pad_id] * self.max_seq_length
         input_ids[: len(hypothesis_ids)] = hypothesis_ids
-        input_ids = np.array(input_ids)
-        return input_ids
+        return np.array(input_ids)
 
     def _get_hypothesis_input_mask(self, idx):
         hypothesis_ids = self.hypotheses_ids[idx]
