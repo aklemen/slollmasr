d7071f6ab1dc22b66a2b54d61976c3e731d27f8e
Anton Klemen <a.klemen@sportradar.com>
2025-08-13 16:05:09 +0200

    Increases LoRA rank and alpha.

--- slurm/fine-tune/whisper-ctc.sh
+++ slurm/fine-tune/whisper-ctc.sh
@@ -1,7 +1,7 @@
 export LLM_NAME="cjvt/GaMS-9B"
 export DATASET_NAME="aklemen/whisper-ctc-h2t"
 export PER_DEVICE_BATCH_SIZE=2
-export LORA_RANK=8
-export LORA_ALPHA=16
+export LORA_RANK=128
+export LORA_ALPHA=64
 
 sbatch --job-name=lora-gams-9b-whisper-ctc fine-tune.sbatch
