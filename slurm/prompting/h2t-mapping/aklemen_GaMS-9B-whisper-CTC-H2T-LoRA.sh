ce58472d0a2c86dde9ee18dd142dcc01a7b1aa1d
Anton Klemen <a.klemen@sportradar.com>
2025-08-17 20:28:17 +0200

    Matches batch size with other methods.

--- slurm/prompting/h2t-mapping/aklemen_GaMS-9B-whisper-CTC-H2T-LoRA.sh
+++ slurm/prompting/h2t-mapping/aklemen_GaMS-9B-whisper-CTC-H2T-LoRA.sh
@@ -1,5 +1,5 @@
 export LLM_NAME="aklemen/GaMS-9B-whisper-CTC-H2T-LoRA"
 export TOKENIZER_NAME=$LLM_NAME
-export BATCH_SIZE=128
+export BATCH_SIZE=64
 
 sbatch --job-name=h2t-mapping-GaMS-9B-whisper-CTC-H2T-LoRA h2t-mapping.sbatch
--- slurm/prompting/h2t-mapping/cjvt_GaMS-9B.sh
+++ slurm/prompting/h2t-mapping/cjvt_GaMS-9B.sh
@@ -1,5 +1,5 @@
 export LLM_NAME="cjvt/GaMS-9B"
 export TOKENIZER_NAME=$LLM_NAME
-export BATCH_SIZE=128
+export BATCH_SIZE=64
 
 sbatch --job-name=h2t-mapping-cjvt-GaMS-9B h2t-mapping.sbatch
