8a021ab5ab6b29b4b07a79b4fb3addb478bffa94
Anton Klemen <a.klemen@sportradar.com>
2025-08-17 22:21:50 +0200

    Adds alphas and betas for masked rescoring.

--- slurm/rescoring/masked-rescore/embeddia_sloberta.sh
+++ slurm/rescoring/masked-rescore/embeddia_sloberta.sh
@@ -1,6 +1,6 @@
 export LLM_NAME="EMBEDDIA/sloberta"
 export TOKENIZER_NAME=$LLM_NAME
-export ALPHAS="" # TODO
-export BETAS="" # TODO
+export ALPHAS="0.095 0.095 0.077 0.159"
+export BETAS="-0.007 -0.159 -0.175 -0.359"
 
 sbatch --job-name=masked-rescore-sloberta ../masked-rescore.sbatch
