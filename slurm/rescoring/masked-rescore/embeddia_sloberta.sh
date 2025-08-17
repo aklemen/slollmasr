2e8cbf61a34a69c7fbeff6ffdf9a2407ee548ebc
Anton Klemen <a.klemen@sportradar.com>
2025-08-17 22:23:08 +0200

    Fixes path.

--- slurm/rescoring/masked-rescore/embeddia_sloberta.sh
+++ slurm/rescoring/masked-rescore/embeddia_sloberta.sh
@@ -3,4 +3,4 @@ export TOKENIZER_NAME=$LLM_NAME
 export ALPHAS="0.095 0.095 0.077 0.159"
 export BETAS="-0.007 -0.159 -0.175 -0.359"
 
-sbatch --job-name=masked-rescore-sloberta ../masked-rescore.sbatch
+sbatch --job-name=masked-rescore-sloberta masked-rescore.sbatch
