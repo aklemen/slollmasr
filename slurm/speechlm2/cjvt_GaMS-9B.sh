37353031a1f0d7fe4632bd7d4c5e3036e07972ad
Anton Klemen <a.klemen@sportradar.com>
2025-08-21 13:14:37 +0200

    Adds SpeechLLM script for GaMS-9B.

---
+++ slurm/speechlm2/cjvt_GaMS-9B.sh
@@ -0,0 +1,11 @@
+export LLM_NAME="cjvt/GaMS-9B"
+export PER_DEVICE_BATCH_SIZE=8
+export TARGET_EFFECTIVE_BATCH_SIZE=128
+
+JOB_NAME=$(echo $LLM_NAME | sed 's/\//-/g' | sed 's/_/-/g')
+sbatch \
+  --job-name="$JOB_NAME" \
+  --ntasks-per-node=4 \
+  --gpus-per-node=H100:4 \
+  --mem-per-gpu=128G \
+  train_speechlm2_gemma.sbatch
