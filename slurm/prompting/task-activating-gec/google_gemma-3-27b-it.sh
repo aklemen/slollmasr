1a60ef91a508ade8dce75322238bbde79ca1e7f1
Anton Klemen <a.klemen@sportradar.com>
2025-08-23 13:12:43 +0200

    Removes Gemma 3 scripts as I won't use them.

--- slurm/prompting/task-activating-gec/google_gemma-3-27b-it.sh
+++
@@ -1,5 +0,0 @@
-export LLM_NAME="google/gemma-3-27b-it"
-export TOKENIZER_NAME=$LLM_NAME
-export BATCH_SIZE=32
-
-sbatch --job-name=task-activating-gec-gemma-3-27b-it task-activating-gec.sbatch
--- slurm/rescoring/causal-rescore/google_gemma-3-12b-pt.sh
+++
@@ -1,6 +0,0 @@
-export LLM_NAME="google/gemma-3-12b-pt"
-export TOKENIZER_NAME=$LLM_NAME
-export ALPHAS="0.423 0.405 0.423 0.409"
-export BETAS="0.104 0.158 0.229 0.221"
-
-sbatch --job-name=causal-rescore-gemma-3-12b-pt causal-rescore.sbatch
