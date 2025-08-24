f9d9029d54425341dee5f00e76d02bae9f55e041
Anton Klemen <a.klemen@sportradar.com>
2025-08-24 23:59:00 +0200

    Adds SlovenianGPT scripts for H2T.

---
+++ training/h2t/slurm/gordicaleksa_SlovenianGPT_ctc.sh
@@ -0,0 +1,12 @@
+export LLM_NAME="aklemen/SlovenianGPT"
+export DATASET_NAME="aklemen/ctc-h2t"
+export MODEL_NAME="SlovenianGPT-CTC-H2T-LoRA"
+
+export PER_DEVICE_BATCH_SIZE=2
+export TARGET_EFFECTIVE_BATCH_SIZE=128
+
+export LORA_RANK=128
+export LORA_ALPHA=64
+
+JOB_NAME="train-h2t-$MODEL_NAME"
+sbatch --job-name="$JOB_NAME" finetune_h2t.sbatch
--- training/h2t/slurm/gordicaleksa_SlovenianGPT_whisper-ctc.sh
+++ training/h2t/slurm/gordicaleksa_SlovenianGPT_whisper-ctc.sh
@@ -1,8 +1,8 @@
-export LLM_NAME="cjvt/GaMS-27B"
+export LLM_NAME="aklemen/SlovenianGPT"
 export DATASET_NAME="aklemen/whisper-ctc-h2t"
-export MODEL_NAME="GaMS-27B-whisper-CTC-H2T-LoRA"
+export MODEL_NAME="SlovenianGPT-whisper-CTC-H2T-LoRA"
 
-export PER_DEVICE_BATCH_SIZE=1
+export PER_DEVICE_BATCH_SIZE=2
 export TARGET_EFFECTIVE_BATCH_SIZE=128
 
 export LORA_RANK=128
