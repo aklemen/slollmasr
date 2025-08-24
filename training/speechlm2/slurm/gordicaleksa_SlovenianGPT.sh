51f0af8672c3b3023e1dd300aa78146e2027c0e6
Anton Klemen <a.klemen@sportradar.com>
2025-08-24 23:53:57 +0200

    Adds SlovenianGPT script for SpeechLM.

---
+++ training/speechlm2/slurm/gordicaleksa_SlovenianGPT.sh
@@ -0,0 +1,12 @@
+export LLM_NAME="aklemen/SlovenianGPT"
+export PROMPT_FORMAT="mistral"
+export PER_DEVICE_BATCH_SIZE=8
+export TARGET_EFFECTIVE_BATCH_SIZE=128
+
+JOB_NAME="speechlm2-$(basename "$LLM_NAME")"
+sbatch \
+  --job-name="$JOB_NAME" \
+  --ntasks-per-node=4 \
+  --gpus-per-node=H100:4 \
+  --mem-per-gpu=128G \
+  train_speechlm2.sbatch
