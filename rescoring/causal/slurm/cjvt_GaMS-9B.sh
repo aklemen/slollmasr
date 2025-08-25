0d649af405042c0cf4e37807ab40c9052cfd134f
Anton Klemen <a.klemen@sportradar.com>
2025-08-25 10:10:07 +0200

    Adds alphas and betas for GaMS-9B.

--- rescoring/causal/slurm/cjvt_GaMS-9B.sh
+++ rescoring/causal/slurm/cjvt_GaMS-9B.sh
@@ -1,7 +1,7 @@
 export LLM_NAME="cjvt/GaMS-9B"
 export TOKENIZER_NAME=$LLM_NAME
-export ALPHAS="" # TODO
-export BETAS="" # TODO
+export ALPHAS="0.683 0.698 0.698 0.695"
+export BETAS="0.235 0.286 0.286 0.481"
 export METHOD="causal-rescore"
 
 JOB_NAME="$METHOD-$(basename "$LLM_NAME")"
