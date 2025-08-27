7c0ce38732a1953f02b954d650b5b610dcce82a3
Anton Klemen <a.klemen@sportradar.com>
2025-08-27 12:23:27 +0200

    Fixes passing EXTRA_EOS_TOKENS variable.

--- speechlm/slurm/aklemen_conformer-ctc-gams-9b.sh
+++ speechlm/slurm/aklemen_conformer-ctc-gams-9b.sh
@@ -1,5 +1,5 @@
 export LLM_NAME="aklemen/conformer-ctc-gams-9b"
-export EXTRA_EOS_TOKENS=("<end_of_turn>")
+export EXTRA_EOS_TOKENS="<end_of_turn>"
 
 JOB_NAME="speechlm-$(basename "$LLM_NAME")"
 sbatch --job-name="$JOB_NAME" speechlm.sbatch
