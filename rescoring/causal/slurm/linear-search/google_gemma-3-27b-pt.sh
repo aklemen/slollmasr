1c830de5228b6caf26dbcf39ec5d4349b575d4a9
Anton Klemen <a.klemen@sportradar.com>
2025-08-28 14:45:21 +0200

    Adds scripts for Gemma 3 27b.

---
+++ rescoring/causal/slurm/linear-search/google_gemma-3-27b-pt.sh
@@ -0,0 +1,8 @@
+export SHOULD_LINEAR_SEARCH_ARTUR_DEV=true
+
+export LLM_NAME="google/gemma-3-27b-pt"
+export TOKENIZER_NAME=$LLM_NAME
+export METHOD="causal-rescore"
+
+JOB_NAME="$METHOD-$(basename "$LLM_NAME")"
+sbatch --job-name="$JOB_NAME" ../../../rescoring.sbatch
