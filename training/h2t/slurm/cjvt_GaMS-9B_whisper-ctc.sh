654920c2d5dd0e4bba3e9c445a452b4e5e4f2b43
Anton Klemen <a.klemen@sportradar.com>
2025-08-30 11:25:39 +0200

    Increases LoRA rank.

--- training/h2t/slurm/cjvt_GaMS-9B_whisper-ctc.sh
+++ training/h2t/slurm/cjvt_GaMS-9B_whisper-ctc.sh
@@ -4,7 +4,7 @@ export DATASET_NAME="aklemen/whisper-ctc-h2t"
 export PER_DEVICE_BATCH_SIZE=2
 export TARGET_EFFECTIVE_BATCH_SIZE=128
 
-export LORA_RANK=64
+export LORA_RANK=256
 export LORA_ALPHA=64
 
 export MODEL_NAME="gams-9b-whisper-ctc-h2t-$LORA_RANK-$LORA_ALPHA"
