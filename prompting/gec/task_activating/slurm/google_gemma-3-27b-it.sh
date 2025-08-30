ee6c35871caf8c4b372c336c898a1f50ef0a9ad7
Anton Klemen <a.klemen@sportradar.com>
2025-08-30 13:33:12 +0200

    Uses smaller batch size for 27b model.

--- prompting/gec/task_activating/slurm/google_gemma-3-27b-it.sh
+++ prompting/gec/task_activating/slurm/google_gemma-3-27b-it.sh
@@ -1,6 +1,7 @@
 export LLM_NAME="google/gemma-3-27b-it"
 export TOKENIZER_NAME=$LLM_NAME
 export METHOD="task-activating-gec"
+export BATCH_SIZE=8
 
 JOB_NAME="$METHOD-$(basename "$LLM_NAME")"
 sbatch --job-name="$JOB_NAME" ../../../prompting.sbatch
--- prompting/selection/zero_shot/slurm/google_gemma-3-27b-it.sh
+++ prompting/selection/zero_shot/slurm/google_gemma-3-27b-it.sh
@@ -1,6 +1,7 @@
 export LLM_NAME="google/gemma-3-27b-it"
 export TOKENIZER_NAME=$LLM_NAME
 export METHOD="zero-shot-selection"
+export BATCH_SIZE=8
 
 JOB_NAME="$METHOD-$(basename "$LLM_NAME")"
 sbatch --job-name="$JOB_NAME" ../../../prompting.sbatch
