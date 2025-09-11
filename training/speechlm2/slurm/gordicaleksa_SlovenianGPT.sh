afae8e137d946c0133a3138516a23452fe39b4ae
Anton Klemen <a.klemen@sportradar.com>
2025-09-11 19:49:53 +0200

    Adjusts to currently available hardware.

--- training/speechlm2/slurm/gordicaleksa_SlovenianGPT.sh
+++ training/speechlm2/slurm/gordicaleksa_SlovenianGPT.sh
@@ -7,6 +7,6 @@ JOB_NAME="train-speechlm2-$(basename "$LLM_NAME")"
 sbatch \
   --job-name="$JOB_NAME" \
   --ntasks-per-node=4 \
-  --gpus-per-node=H100:4 \
+  --gpus-per-node=A100_80GB:4 \
   --mem-per-gpu=128G \
   train_speechlm2.sbatch
