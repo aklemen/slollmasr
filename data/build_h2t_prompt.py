71d8fd1cccad5c1780b77ddb0d8e63511df1b06f
Anton Klemen <a.klemen@sportradar.com>
2025-11-06 21:52:39 +0100

    Fixes typo.

--- data/build_h2t_prompt.py
+++ data/build_h2t_prompt.py
@@ -1,6 +1,6 @@
 prompt_template = ("### Navodilo:\n"
                    "Spodaj je najboljša hipoteza, ki jo je za avdio posnetek generiral sistem za razpoznavanje govora. "
-                   "Preglej jo in jo s pomočjo ostalih hipotez popravi, če je potebno. "
+                   "Preglej jo in jo s pomočjo ostalih hipotez popravi, če je potrebno. "
                    "Potem izpiši končni transkript.\n\n"
                    "### Najboljša hipoteza:\n{best_hypothesis}\n\n"
                    "### Ostale hipoteze:\n{other_hypotheses}\n\n"
