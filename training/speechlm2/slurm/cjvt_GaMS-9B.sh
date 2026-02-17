ca788c39591dc58ec4313816a0fa435c432dd58a
Anton Klemen <a.klemen@sportradar.com>
2026-02-17 23:30:47 +0100

    Adjusts hardware and batch size to avoid waiting in queue.

--- training/speechlm2/slurm/cjvt_GaMS-9B.sh
+++ training/speechlm2/slurm/cjvt_GaMS-9B.sh
@@ -1,6 +1,6 @@
 export LLM_NAME="cjvt/GaMS-9B"
 export PROMPT_FORMAT="gemma"
-export PER_DEVICE_BATCH_SIZE=8
+export PER_DEVICE_BATCH_SIZE=2
 export TARGET_EFFECTIVE_BATCH_SIZE=64
 
 export LORA_RANK=128
@@ -10,6 +10,6 @@ JOB_NAME="train-speechlm2-$(basename "$LLM_NAME")"
 sbatch \
   --job-name="$JOB_NAME" \
   --ntasks-per-node=4 \
-  --gpus-per-node=H100:4 \
+  --gpus-per-node=A100:4 \
   --mem-per-gpu=128G \
   train_speechlm2.sbatch
